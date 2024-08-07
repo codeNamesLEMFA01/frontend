8fddb1bc3bfd0d5d7a9460379b8ac502de70de6b
kohai-fred <frederick.vu.pro@gmail.com>
2024-08-07 18:29:06 +0200

    add Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,17 @@
-import { Suspense, useEffect, useState } from "react"
+import { Box } from "@mui/material"
 
-import { Box, Typography } from "@mui/material"
-
-import { getTotalBySex } from "@src/services/api/names.services"
-import { useQuery } from "@tanstack/react-query"
-import Plot from "react-plotly.js"
-
-import "./App.css"
-
-interface IObjAcc {
-  x: string[]
-  yM: number[]
-  yF: number[]
-}
+import TotalComponent from "./components/TotalComponent/TotalComponent"
+import TrendsByNameComponent from "./components/TrendsComponent/TrendsByNameComponent/TrendsByNameComponent"
+import TrendsTopNamesComponent from "./components/TrendsComponent/TrendsTopNamesComponent/TrendsTopNamesComponent"
+import Hero from "./components/hero/Hero"
 
 function App() {
-  const { data: names } = useQuery({
-    queryKey: ["names"],
-    queryFn: async () => await getTotalBySex(),
-    retry: false,
-    staleTime: Infinity,
-  })
-  const [data, setData] = useState<IObjAcc>()
-
-  useEffect(() => {
-    if (!names) return
-    const obj: IObjAcc = { x: [], yM: [], yF: [] }
-    const data = Object.entries(names.data).reduce((acc, [key, value]) => {
-      acc.yM.push(value.M)
-      acc.yF.push(value.F)
-      acc.x.push(key)
-      return acc
-    }, obj)
-    console.log("🆘 DATA", data)
-    setData(data)
-  }, [names])
-
   return (
     <Box>
-      <Typography variant="h1">Code names!</Typography>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Plot
-          data={[
-            {
-              type: "bar",
-              x: data?.x,
-              y: data?.yM,
-              name: "Male",
-              marker: { color: "118ab2" },
-            },
-            {
-              type: "bar",
-              x: data?.x,
-              y: data?.yF,
-              name: "Female",
-              marker: { color: "ef476f" },
-            },
-          ]}
-          layout={{
-            title: "Total by sex",
-          }}
-          style={{ width: "100%" }}
-        />
-      </Suspense>
+      <Hero />
+      <TotalComponent />
+      <TrendsByNameComponent />
+      <TrendsTopNamesComponent />
     </Box>
   )
 }
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,71 @@
+import { Box, Stack, Typography } from "@mui/material"
+
+import bg from "@assets/hero.medium.jpg"
+import logo from "@assets/logo.png"
+
+const Hero = () => {
+  return (
+    <Stack
+      width={"100vw"}
+      sx={{
+        aspectRatio: "7/ 3",
+        position: "relative",
+        justifyContent: "space-between",
+        boxShadow: "0 3px 12px rgba(0, 0, 0, 0.6)",
+      }}
+    >
+      <Box
+        component={"img"}
+        src={bg}
+        alt=""
+        width={"100%"}
+        sx={{
+          position: "absolute",
+          objectFit: "cover",
+          objectPosition: "center",
+          aspectRatio: "7/ 3",
+          zIndex: -1,
+          filter: "blur(5px)",
+          transform: "scale(1.01)",
+        }}
+      />
+
+      <Stack p={4} direction="row" alignItems="center" justifyContent="center">
+        <Box
+          component={"img"}
+          alt=""
+          src={logo}
+          width={"10vw"}
+          mt={10}
+          sx={{ filter: "grayscale(.5) brightness(.8)" }}
+        />
+        {/* <Typography variant="h1" color={"white"} fontSize={32}>
+          Code names
+        </Typography> */}
+      </Stack>
+
+      <Stack>
+        <Typography
+          component={"a"}
+          href={
+            "https://unsplash.com/fr/photos/photo-en-niveaux-de-gris-des-pieds-dune-personne-xtt_qBVbx6w"
+          }
+          color={"common.white"}
+          fontSize={12}
+          textAlign={"end"}
+          sx={{
+            opacity: 0.5,
+            textDecoration: "none",
+            "&:hover": { opacity: 1, transition: "0.3s" },
+            transition: "0.3s",
+          }}
+          target={"_blank"}
+        >
+          Source: Ryan Graybill Unsplash©
+        </Typography>
+      </Stack>
+    </Stack>
+  )
+}
+
+export default Hero
